0d917a1f19bda0e15b1b1c7f490fdf059a034c17
lugrao <elugrao@gmail.com>
2021-06-10 17:51:25 -0300

    move title to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
+import Head from "next/head"
 import { ChakraProvider } from "@chakra-ui/react"
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <Head>
+        <title>NASA Exoplanet Query</title>
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   )
